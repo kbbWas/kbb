54794dff15774302311ebee4a8ec47e16db7b46c
Gaomeng <32355725+Gaomeng1@users.noreply.github.com>
2018-04-23 15:33:06 +0800

    企业信息

--- kbb_pc/js/qiye_authentication_one.js
+++ kbb_pc/js/qiye_authentication_one.js
@@ -10,22 +10,54 @@ $(document).ready(function ($) {
 
 });
 
-//验证
+//认证
 function shipperAttesation() {
-	$.ajax({
-		url: kbb + "kbb-webs/personalCenter/shipperAttesation",
-		type: 'POST',
-		data:$("#enterpriseForm").serialize(),
-		async: false,
-		cache: false,
-		contentType: false,
-		processData: false,
-		success: function (data) {
-			console.log(data)
-		},
-		error: function (data) {
-			alert(data);
+	$('#enterpriseForm input').change(function () {
+		if($(this).val().length>0){
+			$(this).removeClass('g_border');
 		}
-	});
+	})
+
+	if ($('#g_name').val() == '') {
+		$('#g_name').focus().addClass('g_border').parent().siblings().children('input').removeClass('g_border');
+		return false;
+	} else if ($('#g_address').val() == '') {
+		$('#g_address').focus().addClass('g_border').parent().siblings().children('input').removeClass('g_border');
+		return false;
+	} else if ($('#g_dizhi').val() == '') {
+		$('#g_dizhi').focus().addClass('g_border').parent().siblings().children('input').removeClass('g_border');
+		return false;
+	} else if ($('#g_Telephone').val() == '') {
+		$('#g_Telephone').focus().addClass('g_border').parent().siblings().children('input').removeClass('g_border');
+		return false;
+	} else if ($('#get_mamse').val() == '') {
+		$('#get_mamse').focus().addClass('g_border').parent().siblings().children('input').removeClass('g_border');
+		return false;
+	} else if ($('#g_file_license').val() == '') {
+		$('#g_file_license').focus().addClass('g_border').parent().siblings().children('input').removeClass('g_border');
+		return false
+	} else if ($('#warrant').val() == '') {
+		$('#warrant').focus().addClass('g_border').parent().siblings().children('input').removeClass('g_border');
+		return false
+	} else {
+		$.ajax({
+			url: kbb + "kbb-webs/personalCenter/shipperAttesation",
+			type: 'POST',
+			data: $("#enterpriseForm").serialize(),
+			async: false,
+			cache: false,
+			contentType: false,
+			processData: false,
+			success: function (data) {
+				console.log(data);
+				if(data.status==200){
+					window.location='../html/qiye_authentication_two.html';
+				}
+			},
+			error: function (data) {
+				alert('网络错误');
+			}
+		});
+	}
 
 }
